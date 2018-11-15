848e9b93923161ccf9075a612462c30735ee58a9
ozgurkaracam <ozgurkrcm@hotmail.com>
2018-11-15 13:58:05 +0300

    added bcrypt and Users model.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt=require('bcrypt');
+const User=require('../models/User');
 
-/* GET home page. */
-router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Express' });
+
+router.post('/register', (req, res, next) => {
+
+    bcrypt.hash(req.body.password, 10).then( (hash)=> {
+        const user=new User({
+            username:req.body.username,
+            password:hash
+        });
+        user.save((err,data)=>{
+            if(err)
+                res.json({error:err.message});
+            else
+                res.json({status:'OK'});
+        });
+    });
+
+
 });
 
 module.exports = router;
